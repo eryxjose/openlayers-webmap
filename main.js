4cd312015ff6deee9bc837afaf9e95f937983c22
Eryx <eryxjose@hotmail.com>
2020-08-20 00:48:09 -0300

    Removidas inicialização dos controles.

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 
 window.onload = init; // garante que o javascript seja executado após carregamento completo do documento html
 
-// map controls
-const fullScreenControl = new ol.control.FullScreen();
-const mousePositionControl = new ol.control.MousePosition();
-const overViewMapControl = new ol.control.OverviewMap({
-    collapsed: true,
-    layers: [ // necessário para exibir layer dentro do overlay
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        })
-    ]
-});
-const scaleLineControl = new ol.control.ScaleLine();
-const zoomSliderControl = new ol.control.ZoomSlider();
-const zoomToExtentControl = new ol.control.ZoomToExtent();
-
 function init() {
     
     const map = new ol.Map({
