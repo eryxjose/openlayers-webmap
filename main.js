1dddede83f3cebe76e1d1fd8c667be7f14d1f843
Eryx <eryxjose@hotmail.com>
2020-08-20 13:12:15 -0300

    Propriedade extent com ajuste de zoom e center.

--- main.js
+++ main.js
@@ -5,16 +5,19 @@ function init() {
     
     const map = new ol.Map({
         view: new ol.View({
-            center: [0, 0],
-            zoom: 2,
+            center: [-5885039.681732291, -1139828.9657885488],
+            zoom: 5,
         }),
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM(),  // Open Street Map - https://www.openstreetmap.org
                 zIndex: 1,
                 visible: true, 
-                extent: [] // define a extensão da área visível utilizando: minx miny maxx, maxy
+                // define a extensão da área visível utilizando: minx miny maxx, maxy
+                extent: [-8484041.31321263, -4018303.98758232, -3816343.3818971664, 751049.7970467284], 
+                opacity: 0.5
             }),
+            
         ],
         target: "js-map",
     });
@@ -23,17 +26,20 @@ function init() {
     const layerGroup = new ol.layer.Group({
         layers: [
             new ol.layer.Tile({
+                visible: true, // default true
+                // provavelmente os extents dos layers serão os mesmos
+                extent: [-8484041.31321263, -4018303.98758232, -3816343.3818971664, 751049.7970467284], 
+                opacity: 0.5,
                 source: new ol.source.OSM({
                     // layer OSM Humanitarian
                     url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', 
                     zIndex: 0,
-                    visible: true // default true
                 })
             })
         ]
     });
 
-    map.addLayer(layerGroup);
+   map.addLayer(layerGroup);
 
     map.on('click', function(e) {
         console.log(e.coordinate);
