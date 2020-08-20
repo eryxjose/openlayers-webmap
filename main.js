c7cd4ac6983e29363a6e4242db83fdee8e3d4397
Eryx <eryxjose@hotmail.com>
2020-08-20 01:13:54 -0300

    Adicionado layergroup contendo layer OSM Humanitarian e outras propriedades do layer.

--- main.js
+++ main.js
@@ -10,10 +10,29 @@ function init() {
         }),
         layers: [
             new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
+                source: new ol.source.OSM(),  // Open Street Map - https://www.openstreetmap.org
+                zIndex: 1,
+                visible: true, 
+                extent: [] // define a extensão da área visível utilizando: minx miny maxx, maxy
+            }),
         ],
         target: "js-map",
     });
+
+    // Layer Group
+    const layerGroup = new ol.layer.Group({
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM({
+                    // layer OMS Humanitarian
+                    url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', 
+                    zIndex: 0,
+                    visible: true // default true
+                })
+            })
+        ]
+    });
+
+    map.addLayer(layerGroup);
     
 }
